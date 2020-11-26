4246605e6593bdb121f18bad83cd4c34b30714c3
Rubens Pessoa De Barros Filho <3586658+rubenspessoa@users.noreply.github.com>
2020-11-26 16:09:35 +0100

    init react app

---
+++ web/api/src/app/app.controller.spec.ts
@@ -0,0 +1,18 @@
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {});
+});
