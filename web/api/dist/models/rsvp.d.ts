d9d72635541554b0e673cf28f9d67e4e334bd409
Rubens Pessoa De Barros Filho <3586658+rubenspessoa@users.noreply.github.com>
2020-11-27 13:28:13 +0100

    fix vscode issues

--- web/api/dist/models/rsvp.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Rsvp {
-    id: number;
-    name: string;
-    isGoing: boolean;
-}
-export declare class CreateRsvpDto {
-    name: string;
-    isGoing: boolean;
-}
--- web/api/dist/models/rsvp.schema.d.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from 'mongoose';
-export declare type RsvpDocument = Rsvp & Document;
-export declare class Rsvp {
-    id: number;
-    name: string;
-    isGoing: boolean;
-}
-export declare const RsvpSchema: import("mongoose").Schema<any>;
